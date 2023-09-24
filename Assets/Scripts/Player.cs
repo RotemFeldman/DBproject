083cf1220937f1eb96e1ce36deb099ea174e4a49
RotemFeldman <128305863+RotemFeldman@users.noreply.github.com>
2023-09-24 19:55:44 +0300

    added player class script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Player 
+{
+    public string Id;
+    public string Name;
+    public int Score;
+    public float Time;
+}
